a3ca4a092a90dc2027c4cd350568de2fa45222db
Dilan Perera <r.dilanperera@gmail.com>
2020-06-30 03:16:39 +0530

    Done

--- setup.sh
+++ setup.sh
@@ -1,14 +1,19 @@
 Live Demo
 #!/bin/sh
+b=0
 
-a=0
-
-while [ $a -lt 100 ]
+while [ $b -lt 10 ]
 do
-	echo $[ ( $RANDOM % 100 )  + 1 ]  | tee -a file.txt
-	a=`expr $a + 1`
-done
+	a=0
 
-git add .
-git commit -m "Done"
-git push -u origin master
+	while [ $a -lt 100 ]
+	do
+		echo $[ ( $RANDOM % 100 )  + 1 ]  | tee -a file.txt
+		a=`expr $a + 1`
+	done
+	
+	git add .
+	git commit -m "Done"
+	git push -u origin master
+	b=`expr $b + 1`
+done
