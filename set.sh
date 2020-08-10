6fb609e5698f4a53587df9bfad0700815d18a9ef
RDPerera <r.dilanperera@gmail.com>
2020-08-10 16:21:10 +0530

    Model Trained

--- set.sh
+++ set.sh
@@ -1,20 +1,19 @@
 #!/bin/sh
-
+git config --global credential.helper cache
+git config --global credential.helper 'cache --timeout=720000'
 a=0
 
 while [ $a -lt 10 ]
 do
    echo $a >> file.txt
+   git add .
+   git commit -m "Model Trained"
+   git push
    if [ $a -eq 5 ]
    then
       break
    fi
    a=`expr $a + 1`
 done
-git config --global credential.helper cache
-git config --global credential.helper 'cache --timeout=72000'
-git config --global user.name "RDPerera"
-git config --global user.password "dilan@1998"
-git add .
-git commit -m "Model Trained"
-git push
+
+
